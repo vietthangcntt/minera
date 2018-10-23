ea967941ef8b35ef2ca358725cbdba60ae134927
vietthang <phamvietthangcntt9x@gmail.com>
2018-10-23 10:43:59 +0700

    add responsive woocommerce

--- functions.php
+++ functions.php
@@ -267,6 +267,8 @@ function minera_scripts() {
 	wp_enqueue_script( 'minera-skip-link-focus-fix', get_template_directory_uri() . '/js/skip-link-focus-fix.js', array(), '20151215', true );
 	wp_register_script('js', get_template_directory_uri().'/js/minera.js',array('jquery'));
 	wp_enqueue_script('js');
+	wp_register_style('responsive-css',get_template_directory_uri().'/css/responsive.css','all');
+	wp_enqueue_style('responsive-css');
 
 	if ( is_singular() && comments_open() && get_option( 'thread_comments' ) ) {
 		wp_enqueue_script( 'comment-reply' );
